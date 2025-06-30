b4a8a9fd0b1b8a198299e98a3e4949bb10a5e27e
michaelneuper <michael@michaelneuper.com>
2025-06-30 13:49:38 +0200

    get camera feed from user

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,24 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const app = express();
-app.use(cors());
 
-const port = 5000; // Choose a port different from React's default (3000)
+const port = 8080; // Choose a port different from React's default (3000)
+var corsOptions = {
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  optionsSuccessStatus: 200, // For legacy browser support
+};
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express with TypeScript!');
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express with TypeScript!");
 });
 
-app.get('/api', (req, res) => {
-  res.json({ message: 'TEST API IS WORKING!' });
+app.get("/api", (req, res) => {
+  res.json({ message: "TEST API IS WORKING!" });
 });
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+  console.log(`Server listening at http://0.0.0.0:${port}`);
 });
