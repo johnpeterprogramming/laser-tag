2ef037329ac0985b38a6e09141b13889f08433a5
michaelneuper <michael@michaelneuper.com>
2025-06-30 14:17:15 +0200

    Setup environment variables from frontend and backend.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [react(), basicSsl()],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    
+    return {
+        plugins: [
+            react(),
+            ...(mode === 'development' ? [basicSsl()] : [])
+        ],
+        server: {
+            host: '0.0.0.0',
+            port: Number(env.VITE_PORT) || 5173,
+        },
+    };
 });
