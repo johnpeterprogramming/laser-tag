7ebcbb532cc994ab7154ba30c8291fd284532e99
Takalani Mukwevho <2021475823@ufs4life.ac.za>
2025-06-30 13:23:11 +0200

    Added start page ui.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import PlayerView from './views/PlayerView'
 import SpectatorView from './views/SpectatorView'
+import StartPage from './views/StartPage';
 
 export default function App() {
   return (
     <Routes>
+      <Route path="/" element={<StartPage />} />
       <Route path="/player" element={<PlayerView />} />
       <Route path="/spectator" element={<SpectatorView />} />
     </Routes>
